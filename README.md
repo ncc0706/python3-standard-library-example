7b387c3c4799042ca2ec03c5f7b0dede918a8219
niuyuxian <ncc0706@gmail.com>
2019-08-21 17:42:59 +0800

    chapter01

---
+++ README.md
@@ -0,0 +1,5 @@
+# python3标准库
+
+## 开源地址
+
+> https://pymotw.com/3/
---
+++ chapter01/string_capwords.py
@@ -0,0 +1,8 @@
+import string
+
+s = 'The quick brown for jumped over the lazy doc.'
+
+print(s)
+
+# 首字母大写.
+print(string.capwords(s))
